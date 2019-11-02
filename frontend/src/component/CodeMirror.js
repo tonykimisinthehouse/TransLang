0939086c3fd0f70ab08a330709481522faceba4b
Tony Kim <tonykimisinthehouse@gmail.com>
2019-11-02 15:12:51 -0400

    Added line highlight, python mode, and gutter arrow to the editor

--- frontend/src/component/CodeMirror.js
+++ frontend/src/component/CodeMirror.js
@@ -1,24 +1,65 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import {Controlled as CodeMirror} from 'react-codemirror2'
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/theme/neat.css');
+require('codemirror/mode/python/python.js');
+require('codemirror/addon/selection/active-line.js')
  
-const options = {};
 
-class CodeMirrorComponent extends React.Component {
-    render() {
-        return (
 
-<CodeMirror
-  value={this.state.value}
+const options = {
+  mode: 'python',
+  theme: 'material',
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  
+  // readOnly: 'noCursor',
+  gutters: ["CodeMirror-linenumbers", "breakpoints"]
+};
+
+function CodeMirrorComponent(props) {
+  let defaultValue = 
+  `
+  Spring-2015-Q3.txt
+  `
+
+  const [value, setValue] = useState(
+    defaultValue);
+  const readOnly = props.readOnly;  
+  if (readOnly) {
+    options.readOnly = 'noCursor'
+  }
+
+  function makeMarker() {
+    var marker = document.createElement("div");
+    marker.style.color = "#fff";
+    marker.innerHTML = "⮕";
+    return marker;
+  }
+
+  return (
+    <CodeMirror
+  value={value}
   options={options}
   onBeforeChange={(editor, data, value) => {
-    this.setState({value});
+    setValue(value);
   }}
-  onChange={(editor, data, value) => {
+  onGutterClick={(editor, data, value) => {
+    let info = editor.lineInfo(data);
+    console.log("Line", data + 1, " clicked")
+    console.log("Line info:", info);
+    editor.setGutterMarker(data, "breakpoints", info.gutterMarkers ? null : makeMarker());
+  }}
+  onChange={(editor, value) => {
+    console.log('controlled', {value});
   }}
 />
-        )
-    }
+  )
+
+
 }
 
 export default CodeMirrorComponent;
