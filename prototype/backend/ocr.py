4cb49add908944861c5ea2526e276d9f65cb239c
Tony Kim <tonykimisinthehouse@gmail.com>
2019-11-18 12:39:41 -0500

    added 0.0.1 version of OCR

--- prototype/backend/ocr.py
+++ prototype/backend/ocr.py
@@ -11,5 +11,10 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-i', "--image", required=True, help="path to import image file")
 args = vars(ap.parse_args())
 
+# load the image from disk
+image = cv.imread(args["image"])
+
+text = pytesseract.image_to_string(image)
+print(text)
 
 
