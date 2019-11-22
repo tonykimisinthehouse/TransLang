6de8648c505a35248c3a47e0dced2dec592611e4
marianamatias <mmatias323@gmail.com>
2019-11-21 20:01:10 -0500

    read and write

--- node-js/test.js
+++ node-js/test.js
@@ -1,44 +1,10 @@
-// const { spawn } = require('child_process');
-// // const request = require('request');
-// const test = require('tape');
-
-// // Start the app
-// const env = Object.assign({}, process.env, {PORT: 5000});
-// const child = spawn('node', ['index.js'], {env});
-//client id: 242585418173-3ubkmunjbh03urabfqgnbpgark67fsc3.apps.googleusercontent.com
-//client secret= QQqEXyWvqmVAX2E0jajp6SNT
-
-console.log("Hello");
-
-// test('responds to requests', (t) => {
-//   t.plan(4);
-
-//   // Wait until the server is ready
-//   child.stdout.on('data', _ => {
-//     // Make a request to our app
-//     request('http://127.0.0.1:5000', (error, response, body) => {
-//       // stop the server
-//       child.kill();
-
-//       // No error
-//       t.false(error);
-//       // Successful response
-//       t.equal(response.statusCode, 200);
-//       // Assert content checks
-//       t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-//       t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
-//     });
-//   });
-// });
-
-
 /*******************************************     Google Sheets          ************************************************/
-fs              = require('fs'),
-readline        = require('readline'),
-{google}        = require('googleapis');
+const {google}        = require('googleapis');
+fs              = require('fs');
+readline        = require('readline');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -48,7 +14,8 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
+  authorize(JSON.parse(content), addTranscript);
+  authorize(JSON.parse(content), getTranscript);
 });
 
 /**
@@ -102,21 +69,20 @@ function getNewToken(oAuth2Client, callback) {
 }
 
 /**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * Gets transcripts
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function listMajors(auth) {
+function getTranscript(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
     spreadsheetId: '1l5BsM8-ejfncXU9uLz8kn6DD4YLTGT3_pyra4LJZMto',
-    range: 'A2:E2',
+    range: 'A2:E1000',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
     if (rows.length) {
-      console.log('Try:');
       // Print column A
+      console.log(rows);
       rows.map((row) => {
         console.log(`${row[0]}: ${row[2]}`);
       });
@@ -124,84 +90,33 @@ function listMajors(auth) {
       console.log('No data found.');
     }
   });
-}
+};
 
 
- /**
-   * Gets cell values from a Spreadsheet.
-   * @param {string} spreadsheetId The spreadsheet ID.
-   * @param {string} range The sheet range.
-   * @return {Promise<ValueResponse>} The value response.
-   */
-  function getValues(spreadsheetId, range) {
-    return new Promise((resolve, reject) => {
-      // [START sheets_get_values]
-      this.sheetsService.spreadsheets.values.get({
-        spreadsheetId,
-        range,
-      }, (err, result) => {
-        if (err) {
-          // Handle error
-          console.log(err);
-          // [START_EXCLUDE silent]
-          reject(err);
-          // [END_EXCLUDE]
-        } else {
-          const numRows = result.values ? result.values.length : 0;
-          console.log(`${numRows} rows retrieved.`);
-          // [START_EXCLUDE silent]
-          resolve(result);
-          // [END_EXCLUDE]
-        }
-      });
-      // [END sheets_get_values]
-    });
-  };
+/**
+ * Gets transcripts
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function addTranscript(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.update({
+    spreadsheetId: '1l5BsM8-ejfncXU9uLz8kn6DD4YLTGT3_pyra4LJZMto',
+    range: 'A3',
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      "values": [
+        ['tony', 'korean', 'spanish', 'chinese', 'arabic'],
+        ['haard', 'k', 's', 'c', 'a']
+      ]
+    },
+    auth: auth
+  }, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-
-  /**
-   * Appends values in a Spreadsheet.
-   * @param {string} spreadsheetId The spreadsheet ID.
-   * @param {string} range The range of values to append.
-   * @param {object} valueInputOption Value input options.
-   * @param {(string[])[]} _values A 2d array of values to append.
-   * @return {Promise} The appended values response.
-   */
-  function appendValues(spreadsheetId, range, valueInputOption, _values) {
-    return new Promise((resolve, reject) => {
-      // [START sheets_append_values]
-      let values = [
-        [
-          // Cell values ...
-        ],
-        // Additional rows ...
-      ];
-      // [START_EXCLUDE silent]
-      values = _values;
-      // [END_EXCLUDE]
-      let resource = {
-        values,
-      };
-      this.sheetsService.spreadsheets.values.append({
-        spreadsheetId,
-        range,
-        valueInputOption,
-        resource,
-      }, (err, result) => {
-        if (err) {
-          // Handle error.
-          console.log(err);
-          // [START_EXCLUDE silent]
-          reject(err);
-          // [END_EXCLUDE]
-        } else {
-          console.log(`${result.updates.updatedCells} cells appended.`);
-          // [START_EXCLUDE silent]
-          resolve(result);
-          // [END_EXCLUDE]
-        }
-      });
-      // [END sheets_append_values]
-    });
-  }
-
+    // TODO: Change code below to process the `response` object:
+    console.log(JSON.stringify(res, null, 2));
+  });
+};
